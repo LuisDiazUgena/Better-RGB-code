babe5fc813508546edbbbdd496f484e28043bfcc
Luis Diaz <luisdiazugena@gmail.com>
2015-03-10 10:13:52 +0100

    modified code to add a smooth fade from last to new colours.

--- rgb.ino
+++ rgb.ino
@@ -1,6 +1,7 @@
-//Declaramos las variables globales
+
 int redPin = 6, greenPin = 5, bluePin = 3;
 int delayLed = 10; // Tiempo que pasará entre cada cambio de LED (ms)
+int red_old = 0, green_old = 0, blue_old = 0;
 
 void setup() {
   // Iniciar el puerto serie.
@@ -10,16 +11,15 @@ void setup() {
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
 
+  Serial.println("¡Listo!");
 }
 
 void loop() {
 
-  //Declaramos las variables locales
-  int red=0, green=0, blue=0;
-  int red_old=0,green_old=0,blue_old=0;
-
   // Si hay informacion disponible en el puerto serie la utilizamos:
   while (Serial.available() > 0) {
+    
+    int red = 0, green = 0, blue = 0;
 
     // busca la siguiente cadena de números enteros valida:
     red = Serial.parseInt();
@@ -36,46 +36,43 @@ void loop() {
       //Para leds de anodo común utiliza, para rojo, por ejemplo: red = 255 - constrain(red, 0, 255);
       red = constrain(red, 0, 255);
       green = constrain(green, 0, 255);
-      blue = constrain(blue, 0, 255); 
+      blue = constrain(blue, 0, 255);
 
       // print the three numbers in one string as hexadecimal:
       Serial.print(red, HEX);
       Serial.print(green, HEX);
       Serial.println(blue, HEX);
+
+      //Modificamos el color del led RGB
+      fade(redPin, red, red_old);
+      fade(greenPin, green, green_old);
+      fade(bluePin, blue, blue_old);
+      
+      Serial.println("Color conseguido");
+      //Almacenamos los datos de color anteriores.
+      red_old = red;
+      green_old = green;
+      blue_old = blue;
     }
   }
-
-  //Modificamos el color del led RGB
-  fade(redPin,red,red_old);
-  fade(greenPin,green,green_old);
-  fade(bluePin,blue,blue_old);
-
-  //Almacenamos los datos de color anteriores.
-  red_old = red;
-  green_old = green;
-  blue_old = blue;
-
-
 }
 
-void fade(int pin,int newValue, int aktValue){
+void fade(int pin, int newValue, int aktValue) {
 
   //  Esta funcion se encarga de llevar cada color desde su valor actual hasta el siguiente
   //  Distinguimos dos casos:
   //  1 - El nuevo valor es mayor que el valor actual, por lo que hay que aumentarlo
   //  2 - El nuevo valor es menor que el valor actual, por lo que hay que reducirlo
 
-  if (newValue>aktValue){
-    for(int i=aktValue; i<newValue; i++){
+  if (newValue > aktValue) {
+    for (int i = aktValue; i < newValue; i++) {
       analogWrite(pin, i);
       delay(delayLed);
     }
-    }else if(newValue<aktValue){
-      for (int i=aktValue;i>newValue;i--){
-        analogWrite(pin, i);
-        delay(delayLed);
-      }
-    }else{
-      Serial.println("El valor es igual.");
+  } else if (newValue < aktValue) {
+    for (int i = aktValue; i > newValue; i--) {
+      analogWrite(pin, i);
+      delay(delayLed);
     }
   }
+}
